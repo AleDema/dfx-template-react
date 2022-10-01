f315e763d5969b8b2be144e7c881fb8c42f3d588
Zane <alessandro.demaio@virgilio.it>
2022-10-02 01:08:16 +0200

    tailwind init

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -6,46 +6,22 @@ import { ConnectButton, ConnectDialog, Connect2ICProvider, useConnect } from "@c
 
 
 const App = () => {
-    const [greeting, setGreeting] = React.useState("");
-    const [pending, setPending] = React.useState(false);
-    const inputRef = React.useRef();
 
     const { isConnected, principal, activeProvider } = useConnect({
       onConnect: () => {
         // Signed in
+        console.log("connected!")
       },
       onDisconnect: () => {
         // Signed out
+        console.log("disconnected!")
       }
     })
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (pending) return;
-        setPending(true);
-        const name = inputRef.current.value.toString();
-
-        // Interact with backend actor, calling the greet method
-        const greeting = await backend.greet(name);
-        setGreeting(greeting);
-        setPending(false);
-        return false;
-    }
-
     return (
       <main>
-
-      <ConnectButton />
-      <ConnectDialog dark={false} />
-        <img src={logo} alt="DFINITY logo" />
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Enter your name: &nbsp;</label>
-          <input id="name" alt="Name" type="text" ref={inputRef} />
-          <button id="clickMeBtn" type="submit" disabled={pending}>
-            Click Me!
-          </button>
-        </form>
-        <section id="greeting">{greeting}</section>
+        <ConnectButton />
+        <ConnectDialog />
       </main>
     );
 }
